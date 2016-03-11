b04f6e85aba1811f9fb13896caba07dcb8d62853
kendell <kendell.r@outlook.com>
2016-03-11 13:54:52 -0800

    Update browser_extension.js

--- browser_extension.js
+++ browser_extension.js
@@ -42,7 +42,9 @@
     };
 
     ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+        } else {
+            return {status: 2, msg: 'Ready'};
+        }
     };
 
     var descriptor = {
